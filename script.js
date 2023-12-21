672eefbd85961628227f530999b95c0c96afcf3d
KOTY <96949970+kakotyanimesh@users.noreply.github.com>
2023-12-22 05:28:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+    //navbar js 
+    const menuIcon = document.querySelector(".toggle");
+    const mobileMenu = document.querySelector(".navMenu");
+    
+    menuIcon.onclick = function(){
+        if(mobileMenu.style.display != "block"){
+            mobileMenu.style.display = "block";
+            menuIcon.classList.replace("uil-bars", "uil-multiply")
+        }
+        else{
+            mobileMenu.style.display = "none";
+            menuIcon.classList.replace("uil-multiply", "uil-bars")
+        }
+    }
